569a973ed3dd7d0848cdb3133361d1a00075e1c4
Your Name <you@example.com>
2024-12-26 11:15:56 +0700

    fix build

--- app/src/main/java/com/example/frank/vlcdemo/MainActivity.java
+++ app/src/main/java/com/example/frank/vlcdemo/MainActivity.java
@@ -8,38 +8,12 @@ import android.widget.ImageView;
 
 import java.nio.Buffer;
 
-import vlc.awl.jp.RtspSurfaceRender;
 
 public class MainActivity extends Activity {
 
     public static final String URL = "rtsp://admin:L23FF105@116.110.52.161:554/cam/realmonitor?channel=1&subtype=0";
 //    public static final String URL = "rtsp://root:dsoft123@192.168.4.24/axis-media/media.amp";
 
-    private RtspSurfaceRender mRender;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mRender = new RtspSurfaceRender(this);
-        mRender.setRtspUrl(URL);
-
-        mRender.setRtspListener(new RtspSurfaceRender.RtspListener() {
-            @Override
-            public void onReceived(Buffer buffer, int width, int height) {
-                Log.e("widt", width + "/" + height);
-                buffer.rewind();
-                Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-                bitmap.copyPixelsFromBuffer(buffer);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
-                    }
-                });
-            }
-        });
-    }
 
     @Override
     protected void onResume() {
@@ -53,7 +27,6 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onDestroy() {
-        mRender.releasePlayer();
         super.onDestroy();
     }
 }
